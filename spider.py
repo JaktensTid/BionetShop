2253d49b5a6ee44bbc55cbd7e69cbf19449df1e1
JaktensTid <jaktenstid1@gmail.com>
2017-04-20 11:59:45 +0300

    Extracting

--- spider.py
+++ spider.py
@@ -44,15 +44,40 @@ class Spider:
 
         return urls
 
+    # NOT SCRAPED - category 3, 4
     async def fetch(self, product, session):
-        async with session.get(product) as response:
-            text = await response.read()
+        async with session.get(product['webpage']) as response:
+            text = await response.text()
+
             if text:
                 document = html.fromstring(text)
-                ID, CAS, _ = document.xpath(".//div[@class='info']/span[@class='value']/text()")
+                ID, CAS, _ = document.xpath(".//div[@class='delivery-info']//span[@class='value']/text()")
                 product['id'], product['cas'] = ID, CAS
                 product['name'] = document.xpath(".//span[@class='h1']/text()")
-
+                product['category1'] = document.xpath(".//li[@class='category9']/a/text()")
+                product['category2'] = document.xpath(".//li[@class='category11']/a/text()")
+                product['availability'] = document.xpath(".//div[@class='stock-info']//span[@class='value']/text()")
+                product['delivery'] = document.xpath(".//div[@class='stock-info']//p[position()=2]/text()")
+                prices = document.xpath(".//div[@class='product-shop']/table//tr/td[position()=3]//text()")
+                packs = document.xpath(".//div[@class='product-shop']/table//tr/td[position()=1]/text()")[0:prices+1]
+                product['packs'] = {k:v for k,v in zip(prices, packs)}
+                table = document.xpath(".//div[@class='product-collateral toggle-content tabs']/table")
+                formula = table.xpath(".//tr/td[contains(.,'Formula')]")
+                product['formula'] = formula.replace('Formula', '')
+                purity = table.xpath(".//tr/td[contains(.,'Purity')]")
+                product['purity'] = purity.replace('Purity', '')
+                supplier = table.xpath(".//tr/td[contains(.,'Supplier Name')]").replace('Supplier Name', '')
+                product['supplier'] = supplier
+                supplierid = table.xpath(".//tr/td[contains(.,'Supplier ID')]").replace('Supplier ID', '')
+                product['supplierid'] = supplierid
+                acdno = table.xpath(".//tr/td[contains(.,'ACD no')]").replace('ACD no', '')
+                product['acd no'] = acdno
+                mw = table.xpath(".//tr/td[contains(.,'MW')]").replace('MW', '')
+                product['mw'] = mw
+                synonym = table.xpath(".//tr/td[contains(.,'Synonym')]").replace('Synonym', '')
+                product['synonym'] = synonym
+                product['img'] = document.xpath(".//img[@id='image-main']/@src")
+                product['related'] = document.xpath(".//ul[@id='upsell-product-table']//span[position()=2]/text()")
 
 
             return product
@@ -63,10 +88,10 @@ class Spider:
 
     async def run(self, products):
         tasks = []
-        sem = asyncio.Semaphore(1000)
+        sem = asyncio.Semaphore(20)
 
         async with ClientSession() as session:
-            for url in urls:
+            for product in products:
                 task = asyncio.ensure_future(self.bound_fetch(sem, product, session))
                 tasks.append(task)
 
@@ -76,6 +101,10 @@ class Spider:
 
 if __name__ == '__main__':
     spider = Spider()
-    urls = spider.get_products_urls()
-    pass
+    products = [{'webpage' : 'https://www.keyorganics.net/biochemicals/research-area/oncology/-34.html'},
+                {'webpage' : 'https://www.keyorganics.net/fragments-screening/screening/-15848.html'}]
+    loop = asyncio.get_event_loop()
+
+    future = asyncio.ensure_future(spider.run(products))
+    loop.run_until_complete(future)
 
